ae1f6dbc8381a39afa255c0a214e89b66e1decd3
aymanmilhem <aymanmilhem@gmail.com>
2018-11-20 12:08:29 +1300

    "Second Commit after following along with the book"

--- src/multidimentsionalarray/MultiDimentsionalArray.java
+++ src/multidimentsionalarray/MultiDimentsionalArray.java
@@ -5,6 +5,8 @@
  */
 package multidimentsionalarray;
 
+import java.util.Arrays;
+
 /**
  *
  * @author omega
@@ -16,6 +18,23 @@ public class MultiDimentsionalArray {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int boardDim = 8;
+        char[][] board = new char[boardDim][boardDim];
+        boolean isWhite = false;
+        for (int y = 0; y < board.length; y++)
+        {
+            isWhite = !isWhite;
+            for (int x = 0; x < board[y].length; x++)
+            {
+                if (isWhite) board[y][x] = 'W';
+                if (!isWhite) board[y][x] = 'B';
+                isWhite = !isWhite;
+            }
+        }
+        for (int i = 0; i < board.length; i++)
+        {
+            System.out.println(Arrays.toString(board[i]));
+        }
     }
     
 }
